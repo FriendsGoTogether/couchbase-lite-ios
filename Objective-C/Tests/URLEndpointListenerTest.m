b9b3cfab9360730bb3ad1d27e1f9beb497374c5d
jayahariv <jayahariv88@gmail.com>
2020-03-06 18:44:38 -0800

    * include possible test cases, so that we can validate one by one

--- Objective-C/Tests/URLEndpointListenerTest.m
+++ Objective-C/Tests/URLEndpointListenerTest.m
@@ -18,16 +18,19 @@
 
 @implementation URLEndpointListenerTest
 
-- (CBLURLEndpointListener*) listenTo: (NSString*)network port: (uint16)port {
+#pragma mark - Helper methods
+- (CBLURLEndpointListener*) listenTo: (NSString*)network
+                                port: (uint16)port
+                            database: (CBLDatabase*)database {
     CBLURLEndpointListener* listener;
     CBLURLEndpointListenerConfiguration* config;
     if (network) {
-        config = [[CBLURLEndpointListenerConfiguration alloc] initWithDatabase: otherDB
+        config = [[CBLURLEndpointListenerConfiguration alloc] initWithDatabase: database
                                                                           port: port
                                                               networkInterface: network
                                                                       identity: nil];
     } else {
-        config = [[CBLURLEndpointListenerConfiguration alloc] initWithDatabase: otherDB
+        config = [[CBLURLEndpointListenerConfiguration alloc] initWithDatabase: database
                                                                           port: port
                                                                       identity: nil];
     }
@@ -39,11 +42,13 @@
     return listener;
 }
 
-- (void) testWithPortOnly {
+#pragma mark - Basic Tests
+
+- (void) testCustomPort {
     CBLDatabase.log.console.level = kCBLLogLevelInfo;
     NSString* urlString = [NSString stringWithFormat: @"ws://localhost:5666/%@", otherDB.name];
     NSURL* url = [[NSURL alloc] initWithString: urlString];
-    CBLURLEndpointListener* list = [self listenTo: nil port: 5666];
+    CBLURLEndpointListener* list = [self listenTo: nil port: 5666 database: otherDB];
 
     [self generateDocumentWithID: @"doc-1"];
     CBLURLEndpoint* target = [[CBLURLEndpoint alloc] initWithURL: url];
@@ -53,14 +58,16 @@
     AssertEqual(self.db.count, 1);
     AssertEqual(otherDB.count, 1);
     
+    // TODO: get and verify the ports are same!
+    
     [list stop];
 }
 
-- (void) testWithHostAndPort {
+- (void) testCustomNetworkInterface {
     CBLDatabase.log.console.level = kCBLLogLevelInfo;
     NSString* urlString = [NSString stringWithFormat: @"ws://127.0.0.1:8080/%@", otherDB.name];
     NSURL* url = [[NSURL alloc] initWithString: urlString];
-    CBLURLEndpointListener* list = [self listenTo: url.host port: 8080];
+    CBLURLEndpointListener* list = [self listenTo: url.host port: 8080 database: otherDB];
 
     [self generateDocumentWithID: @"doc-1"];
     CBLURLEndpoint* target = [[CBLURLEndpoint alloc] initWithURL: url];
@@ -70,7 +77,28 @@
     AssertEqual(self.db.count, 1);
     AssertEqual(otherDB.count, 1);
     
+    // TODO: check the custom network interface is same!
+    
     [list stop];
 }
+
+- (void) testMultipleListener { }
+- (void) testListenerWithActiveReplication { }
+- (void) testConflictResolution { }
+
+#pragma mark - Authentication
+
+- (void) testBasicAuthentication { }
+- (void) testIncorrectBasicAuthentication { }
+- (void) testCertificateAuthentication { }
+- (void) testIncorrectCertificateAuthentication { }
+- (void) testTLSIdentity { }
+- (void) testUnAuthorizedAccess { }
+
+#pragma mark - Corner Cases
+
+- (void) testReservedPortAccess { }
+- (void) testIncorrectNetworkInterface { }
+
 
 @end
