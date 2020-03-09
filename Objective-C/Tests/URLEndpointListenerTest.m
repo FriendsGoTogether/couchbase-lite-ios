866fd361781943a48bbebbee6d6e251bd6a42d71
jayahariv <jayahariv88@gmail.com>
2020-03-09 13:21:57 -0700

    * add multiple listeners on different dbs test add

--- Objective-C/Tests/URLEndpointListenerTest.m
+++ Objective-C/Tests/URLEndpointListenerTest.m
@@ -64,7 +64,6 @@
 }
 
 - (void) testCustomNetworkInterface {
-    CBLDatabase.log.console.level = kCBLLogLevelInfo;
     NSString* urlString = [NSString stringWithFormat: @"ws://127.0.0.1:8080/%@", otherDB.name];
     NSURL* url = [[NSURL alloc] initWithString: urlString];
     CBLURLEndpointListener* list = [self listenTo: url.host port: 8080 database: otherDB];
@@ -82,7 +81,38 @@
     [list stop];
 }
 
-- (void) testMultipleListener { }
+- (void) testMultipleListenerOnDifferentDBs {
+    NSMutableArray<CBLURLEndpointListener*>* lists = [NSMutableArray array];
+    NSMutableArray<CBLDatabase*>* dbs = [NSMutableArray array];
+    for (uint16 i = 0; i < 10; i++) {
+        NSString* name = [NSString stringWithFormat: @"dbName-%d", i];
+        NSError* err = nil;
+        CBLDatabase* tempDB = [[CBLDatabase alloc] initWithName: name error: &err];
+        
+        [lists addObject: [self listenTo: @"127.0.0.1" port: 8080 + i database: tempDB]];
+        [dbs addObject: tempDB];
+    }
+    [self generateDocumentWithID: @"doc-1"];
+    
+    for (uint16 i = 0; i < 10; i++) {
+        NSString* urlString = [NSString stringWithFormat: @"ws://127.0.0.1:%d/%@", (8080 + i), [dbs objectAtIndex: i].name];
+        NSURL* url = [[NSURL alloc] initWithString: urlString];
+        CBLURLEndpoint* target = [[CBLURLEndpoint alloc] initWithURL: url];
+        id config = [self configWithTarget: target type: kCBLReplicatorTypePush continuous: NO];
+        [self run: config errorCode: 0 errorDomain: nil];
+    }
+    
+    
+    AssertEqual(self.db.count, 1);
+    for (uint16 i = 0; i < 10; i++) {
+        AssertEqual([dbs objectAtIndex: i].count, 1);
+    }
+    
+    for (uint16 i = 0; i < 10; i++) {
+        [[lists objectAtIndex: i] stop];
+    }
+}
+
 - (void) testListenerWithActiveReplication { }
 - (void) testConflictResolution { }
 
